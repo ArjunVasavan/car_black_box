8041c74e781484bb2d0fd2c6bae1a3e0b4f50d95
Arjun Vasavan <vasavanarjun@gmail.com>
2026-04-24 11:28:37 +0530

    fix(menu): now it prints all 3 screens

--- car_black_box.X/menu.c
+++ car_black_box.X/menu.c
@@ -19,37 +19,44 @@ void display_main_menu(void) {
     const char* menu[] = {
         "VIEW LOG",
         "DOWNLOAD",
+        "CLEAR LOG",
         "SET TIME",
-        "CLEAR LOG"
     };
 
     unsigned char key = read_switches(STATE_CHANGE);
 
     if ( key == MK_SW1 && selected < 3 ) {
-        selected+=1;
-        if ( cursor == 0 ) {
-            cursor = 1;
+        selected += 1;
+        if ( selected > 0 ) {
+            menu_index = selected - 1;
         } else {
+            menu_index = 0;
+        }
+        if ( selected % 2 == 0 ) {
             cursor = 0;
-            menu_index+=1;
+        } else {
+            cursor = 1;
         }
         CLEAR_DISP_SCREEN;
     } else if ( key == MK_SW2 && selected > 0 ) {
-        selected--;
-        if ( cursor == 1 ) {
+        selected -= 1;
+        if ( selected > 0 ) {
+            menu_index = selected - 1;
+        } else {
+            menu_index = 0;
+        }
+        if ( selected % 2 == 0 ) {
             cursor = 0;
         } else {
             cursor = 1;
-            menu_index-=1;
         }
         CLEAR_DISP_SCREEN;
-    } else if (key == MK_SW11) {
+    } else if ( key == MK_SW11 ) {
         if ( selected == 0 ) state = e_view_log;
         if ( selected == 1 ) state = e_download_log;
-        if ( selected == 2 ) state = e_set_time;
-        if ( selected == 3 ) state = e_clear_log;
+        if ( selected == 2 ) state = e_clear_log;
+        if ( selected == 3 ) state = e_set_time;
     } else if ( key == MK_SW12 ) {
-        // reset menu state before leaving
         menu_index = 0;
         cursor = 0;
         selected = 0;
@@ -57,19 +64,16 @@ void display_main_menu(void) {
         CLEAR_DISP_SCREEN;
     }
 
-    //display
-
     if ( cursor == 0 ) {
-        clcd_print((unsigned char*)"->",LINE1(0));
-        clcd_print((unsigned char*)"  ",LINE2(0));
+        clcd_print((unsigned char*)"->", LINE1(0));
+        clcd_print((unsigned char*)"  ", LINE2(0));
     } else {
-        clcd_print((unsigned char*)"  ",LINE1(0));
-        clcd_print((unsigned char*)"->",LINE2(0));
+        clcd_print((unsigned char*)"  ", LINE1(0));
+        clcd_print((unsigned char*)"->", LINE2(0));
     }
 
-    clcd_print((unsigned char*)menu[menu_index],LINE1(3));
-    clcd_print((unsigned char*)menu[menu_index+1],LINE2(3));
-
+    clcd_print((unsigned char*)menu[menu_index],   LINE1(3));
+    clcd_print((unsigned char*)menu[menu_index+1], LINE2(3));
 }
 
 void view_log(void) {
